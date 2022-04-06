ff5db67da534e69d6cddb5b364ba09c49e9eeda4
Johanu66 <85310382+Johanu66@users.noreply.github.com>
2022-04-06 14:50:44 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     //Connexion à la bd
     try{
-        $bdd = new PDO("pgsql:host=ec2-18-210-64-223.compute-1.amazonaws.com;port=5432;dbname=d247ipb6vvqjje;","izkgcpzshxeemb","8d6000fb254111077258a22b963714c5f0131b97bfda5943f4c2ca7ee49f16f2", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $bdd = new PDO("pgsql:host=ec2-18-214-134-226.compute-1.amazonaws.com;port=5432;dbname=d4bocphvnml258;","crogdmraipfgom","e7a078082b7d9f65bfede3ddf64cd2b7677347246bcc696ba621959e35bd956b", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
     catch(Exception $e){
         die('Erreur : '.$e->getMessage());
