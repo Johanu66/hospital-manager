cea0911411a2a8733fb05d32e6561806c7daf421
Johanu <johanugandonou@gmail.com>
2024-09-07 04:17:52 +0000

    Configuration pour la base de données

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     //Connexion à la bd
     try{
-        $bdd = new PDO("mysql:host=localhost;port=3306;dbname=hospital;","root","", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $bdd = new PDO("mysql:host=localhost;port=3306;dbname=tqffjfwm_hospital;","tqffjfwm_hospital","GGGW:ZZb7MGMZwccZ*", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
     catch(Exception $e){
         die('Erreur : '.$e->getMessage());
