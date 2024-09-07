1b4ab098646317e6172878ee596e7199b2c014ba
Johanu GANDONOU <johanugandonou@gmail.com>
2024-09-07 11:00:36 +0200

    Suppression des liens google analytics

--- tb/tb.php
+++ tb/tb.php
@@ -443,15 +443,7 @@
         <script src="../plugins/amcharts/pie.js"></script>
         <script src="../plugins/ammap3/ammap/ammap.js"></script>
         <script src="../plugins/ammap3/ammap/maps/js/usaLow.js"></script>
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='https://www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
+
 
 
         <script>
