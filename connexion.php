2388155957ea257494a11086c4a5bdf1a015072d
Johanu GANDONOU <johanugandonou@gmail.com>
2024-09-19 23:37:53 +0200

    Ajout des mesures de sécurité à la page de connexion

--- connexion.php
+++ connexion.php
@@ -7,11 +7,8 @@
         <meta name="description" content="">
         <meta name="keywords" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
-
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
-        
         <link rel="stylesheet" href="plugins/bootstrap/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
         <link rel="stylesheet" href="plugins/ionicons/dist/css/ionicons.min.css">
@@ -26,46 +23,64 @@
         <?php
             include("db.php");
             include("fonctions_sql.php");
-            $mdp_correct = false;
+
+            session_start(); // Démarrer la session
+
+            // Protection contre CSRF : Génération d'un token à la première visite
+            if (empty($_SESSION['csrf_token'])) {
+                $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+            }
+            $csrf_token = $_SESSION['csrf_token'];
+
+            // Traitement de la soumission du formulaire
             if(isset($_POST["submit"])){
-                $statement = $bdd->prepare("SELECT * FROM compte INNER JOIN personne ON compte.id_personne_fk_compte = personne.id_personne WHERE email_personne = :email");
-                $statement->execute(array(':email'	=>	$_POST["email_personne"]));
-                if($statement->rowCount() > 0){
-                    while($row = $statement->fetch()){
-                        if($_POST["mdp_compte"] == $row["mdp_compte"]){
-                            if($row["statut_compte"] == "Actif"){
-                                if(isset($_POST['remenber_me'])){
-                                    setcookie('email',$row["email_personne"],time()+365*24*3600,null,null,false,true);
-                                    setcookie('mdp',$row["mdp_compte"],time()+365*24*3600,null,null,false,true);
-                                }
-                                session_start();
-                                $_SESSION['id'] = $row['id_personne'];
-                                $_SESSION["nom"] = $row["nom_personne"];
-                                $_SESSION["prenom"] = $row["prenom_personne"];
-                                $_SESSION["email"] = $row["email_personne"];
-                                $_SESSION["id_type_compte"] = $row["id_type_compte_fk_compte"];
-                                $_SESSION["photo"] = $row["photo_personne"];
-                                $_SESSION["menu"] = get_menu_by_compte($row['id_compte']);
-                                header("Location:tb/tb.php");
-                            }
-                            else{
-                                ?>
-                                <script>swal("Échoué","Votre compte est Inatif veuillez contacter l'administrateur.","error") </script>
-                                <?php
-                            }
-                            $mdp_correct = true;
-                        }
-                    }
-                    if($mdp_correct == false){
-                        ?>
-                        <script>swal("Échoué","Identifiant ou mot de passe incorrect.","error") </script>
-                        <?php
-                    }
+                // Vérification du token CSRF
+                if(!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+                    die('Requête CSRF invalide.');
                 }
-                else{
-                    ?>
-                    <script>swal("Échoué","Identifiant ou mot de passe incorrect.","error") </script>
-                    <?php
+
+                // Validation de l'email
+                $email = filter_var($_POST['email_personne'], FILTER_VALIDATE_EMAIL);
+                if(!$email) {
+                    die('Email invalide.');
+                }
+
+                // Préparation de la requête pour vérifier le compte
+                $statement = $bdd->prepare("SELECT * FROM compte INNER JOIN personne ON compte.id_personne_fk_compte = personne.id_personne WHERE email_personne = :email");
+                $statement->execute(array(':email' => $email));
+
+                if($statement->rowCount() > 0){
+                    $row = $statement->fetch();
+
+                    // Vérification du mot de passe haché
+                    if(password_verify($_POST["mdp_compte"], $row["mdp_compte"])){
+                        if($row["statut_compte"] == "Actif"){
+                            // Si "Se rappeler de moi" est coché, définir des cookies sécurisés
+                            if(isset($_POST['remenber_me'])){
+                                setcookie('email', $row["email_personne"], time() + 365*24*3600, '/', null, true, true); // HttpOnly, Secure
+                                setcookie('mdp', $row["mdp_compte"], time() + 365*24*3600, '/', null, true, true); // HttpOnly, Secure
+                            }
+
+                            // Stockage des informations de session
+                            $_SESSION['id'] = $row['id_personne'];
+                            $_SESSION["nom"] = htmlspecialchars($row["nom_personne"], ENT_QUOTES, 'UTF-8');
+                            $_SESSION["prenom"] = htmlspecialchars($row["prenom_personne"], ENT_QUOTES, 'UTF-8');
+                            $_SESSION["email"] = htmlspecialchars($row["email_personne"], ENT_QUOTES, 'UTF-8');
+                            $_SESSION["id_type_compte"] = $row["id_type_compte_fk_compte"];
+                            $_SESSION["photo"] = htmlspecialchars($row["photo_personne"], ENT_QUOTES, 'UTF-8');
+                            $_SESSION["menu"] = get_menu_by_compte($row['id_compte']);
+
+                            // Redirection après connexion réussie
+                            header("Location: tb/tb.php");
+                            exit();
+                        } else {
+                            echo "<script>swal('Échoué', 'Votre compte est inactif. Veuillez contacter l’administrateur.', 'error');</script>";
+                        }
+                    } else {
+                        echo "<script>swal('Échoué', 'Identifiant ou mot de passe incorrect.', 'error');</script>";
+                    }
+                } else {
+                    echo "<script>swal('Échoué', 'Identifiant ou mot de passe incorrect.', 'error');</script>";
                 }
             }
         ?>
@@ -83,22 +98,25 @@
                             <div class="logo-centered">
                                 <a href="connexion.php"><img src="img/logo.png" width="40" alt=""></a>
                             </div>
-                            <h3>Connexion à l'App de gestion hôpitalière</h3>
+                            <h3>Connexion à l'App de gestion hospitalière</h3>
                             <p>Ravi de vous revoir!</p>
                             <form action="" method="POST">
+                                <!-- Token CSRF caché -->
+                                <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
+
                                 <div class="form-group">
-                                    <input type="text" name="email_personne" class="form-control" placeholder="Email" required="" value="claire.dupont@example.com">
+                                    <input type="text" name="email_personne" class="form-control" placeholder="Email" required value="claire.dupont@example.com">
                                     <i class="ik ik-user"></i>
                                 </div>
                                 <div class="form-group">
-                                    <input type="password" name="mdp_compte" class="form-control" placeholder="Password" required="" value="Az3$54Tg">
+                                    <input type="password" name="mdp_compte" class="form-control" placeholder="Mot de passe" required value="12345678">
                                     <i class="ik ik-lock"></i>
                                 </div>
                                 <div class="row">
                                     <div class="col text-left">
                                         <label class="custom-control custom-checkbox">
-                                            <input type="checkbox" class="custom-control-input" id="item_checkbox" name="remenber_me" value="option1">
-                                            <span class="custom-control-label">&nbsp;Se rapeller de moi</span>
+                                            <input type="checkbox" class="custom-control-input" id="item_checkbox" name="remenber_me">
+                                            <span class="custom-control-label">&nbsp;Se rappeler de moi</span>
                                         </label>
                                     </div>
                                     <div class="col text-right">
@@ -114,7 +132,7 @@
                 </div>
             </div>
         </div>
-        
+
         <script src="assets/modules/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="src/js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
         <script src="plugins/popper.js/dist/umd/popper.min.js"></script>
@@ -122,6 +140,5 @@
         <script src="plugins/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
         <script src="plugins/screenfull/dist/screenfull.js"></script>
         <script src="dist/js/theme.js"></script>
-
     </body>
 </html>
