aa510e44faacb9f42459db1bda4904b08a70a4bc
Johanu <johanugandonou@gmail.com>
2024-09-19 22:18:51 +0000

    Details de db sur planethoster

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     //Connexion à la bd
     try{
-        $bdd = new PDO("mysql:host=localhost;port=3306;dbname=hospital;charset=utf8","root","", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $bdd = new PDO("mysql:host=localhost;dbname=tqffjfwm_hospital;charset=utf8","tqffjfwm_hospital","i=ii_iZR_i@11Z^71#", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
     catch(Exception $e){
         die('Erreur : '.$e->getMessage());
